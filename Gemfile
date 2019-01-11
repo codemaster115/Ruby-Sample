d1931aeec3b030fe6c413317802c1ddb0c1a4082
codemaster6152 <punctualityman@outlook.com>
2019-01-11 12:25:37 +1000

    finish user edit, update, index and destroy actions

--- Gemfile
+++ Gemfile
@@ -7,6 +7,11 @@ ruby '2.5.0'
 gem 'rails', '~> 5.2.1'
 gem 'bootstrap-sass', '3.3.7'
 gem 'bcrypt', '3.1.12'
+gem 'faker', '1.7.3'
+
+gem 'will_paginate',           '3.1.6'
+gem 'bootstrap-will_paginate', '1.0.0'
+
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name:  "test",
+  email: "test@example.com",
+  password:              "test1234",
+  password_confirmation: "test1234",
+  admin: true)
+
+99.times do |n|
+name  = Faker::Name.name
+email = "example-#{n+1}@railstutorial.org"
+password = "password"
+User.create!(name:  name,
+    email: email,
+    password:              password,
+    password_confirmation: password)
+end
