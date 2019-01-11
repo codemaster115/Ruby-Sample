cd8c8c78b840b53275930031cd7365a1528cedd3
codemaster6152 <punctualityman@outlook.com>
2019-01-11 15:47:56 +1000

    fixed production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
     :authentication       => :plain,
     :user_name            => ENV['SENDGRID_USERNAME'],
     :password             => ENV['SENDGRID_PASSWORD'],
-    :domain               => 'heroku.com'
+    :domain               => 'heroku.com',
     :enable_stattls_auto  => true
   }
   # Ignore bad email addresses and do not raise email delivery errors.
